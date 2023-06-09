da8a041fbbafa7410d8d54911e810049bd342ee5
unknown <thiagomotta.3000@gmail.com>
2023-06-09 16:45:44 -0300

    Adicionar mais mensagens de erros

--- Modelo/controller_torneio_api_dados.dart
+++ Modelo/controller_torneio_api_dados.dart
@@ -26,37 +26,49 @@ class Placar
 
 enum err_comp_add
 {
+  none,
   nome_duplicado,
   torneio_cheio,
-  torneio_inexistente
+  torneio_inexistente,
+  erro_bd
 }
 
 enum err_pedir_entrada
 {
+  none,
   nome_invalido,
-  codigo_entrada_invalido
+  codigo_entrada_invalido,
+  acesso_negado,
+  erro_bd
 }
 
 enum err_comp_rem
 {
+  none,
+  torneio_inexistente,
   comp_inexistente,
-  torneio_em_progresso
+  torneio_em_progresso,
+  erro_bd
 }
 
 enum err_regras
 {
+  none,
   regras_indeterminadas,    // Regras do torneio não foram escolhidas
   regras_inexistentes,      // Regras selecionadas não existem
-  torneio_em_progresso
+  torneio_em_progresso,
+  torneio_inexistente
 }
 
 enum err_criar_etapa
 {
+  none,
   torneio_invalido,         // Criação da primeira etapa e torneio ainda não preparado
   etapa_nao_concluida
 }
 
 enum err_concluir_etapa
 {
+  none,
   partida_invalida
 }
